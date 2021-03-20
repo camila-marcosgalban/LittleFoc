b28de542abd98360671849f8537d1a77f0da5547
Camila Marcos Galban <camilamarcosgalban@gmail.com>
2021-03-20 00:56:31 -0300

    contador fix

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,32 +1,27 @@
 import React from "react";
 import { useState } from "react";
-import ItemCount from "../components/ItemCount";
+import ItemCount from "./ItemCount";
 
-function Item(props) {
-  const [stockActual, setStockActual] = useState(15);
-  const restarStock = (e, nuevoStock) => {
-    e.preventDefault();
-    if(stockActual !== 0){
-      setStockActual((stockActual) => stockActual - nuevoStock);
-    }else{
-      console.log("no hay stock");
-    }
-    
-  };
+ function Item(props) {
 
-    return (
-      <React.Fragment>
-          <div className="card">
-  <img src={props.item.url} className="card-img-top" alt="..." />
-  <div className="card-body">
-    <h5 className="card-title">{props.item.title}</h5>
-    <p className="card-text">{props.item.description}</p>
-    <p className="card-text">Precio: {props.item.price}</p>
-    <ItemCount stock={stockActual} initial={1} onAdd={restarStock} />
-  </div>
-</div>
-      </React.Fragment>
-    );
+  const onAdd = (e, q) => {
+    alert (`Agregaste ${q} elementos al carrito`)
   }
+
+  return (
+    <React.Fragment>
+        <div className="card">
+<img src={props.item.url} className="card-img-top" alt="..." />
+<div className="card-body">
+  <h5 className="card-title">{props.item.title}</h5>
+  <p className="card-text">{props.item.description}</p>
+  <p className="card-text">Precio: {props.item.price}</p>
+  <ItemCount stock={15} initial={1} onAdd={onAdd} />
+</div>
+</div>
+    </React.Fragment>
+  );
+     }
+
   
   export default Item;
--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,10 +1,41 @@
+import React from "react";
+import { useState } from "react";
+
 const ItemCount = ({ stock, initial, onAdd }) => {
-    return (
-      <form>
-          <p className="mt-1">Stock: {stock}</p>
-        <input id="cantidad" type="number" placeholder={initial}></input>
-        <a href="#" onClick={(e) => onAdd(e, 1)} className="btn btnCardItems mt-2">Agregar</a>
+    
+  const [ counter, setCounter] = useState (initial);
+
+  const btnAdd = () => {
+    if (counter < stock){
+      setCounter (counter + 1);
+    }
+  }
+
+  const btnReset = () => {
+    setCounter (initial);
+  }
+
+  const btnSubstract = () => {
+    if (counter > 1){
+      setCounter (counter - 1);
+    }
+  }
+
+  
+  
+  return (
+    <React.Fragment>
+      <div className="mb-3" >
+      <button onClick={btnSubstract} className="btn btnCardItems mt-2 mx-1">-</button>
+      <button href="#" onClick={btnReset} className="btn btnCardItems mt-2 mx-1">Resetear</button>
+      <button href="#" onClick={btnAdd} className="btn btnCardItems mt-2 mx-1">+</button>
+        </div>
+
+      <form onSubmit={ e=> onAdd(e,counter)}>
+          <input className="mx-2" value={counter} />
+          <button type='submit'>Add</button>
       </form>
+    </React.Fragment>
     );
   };
   
